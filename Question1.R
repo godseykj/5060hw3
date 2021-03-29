30cd38a186e3e32739dac43f42827435f3753bff
godseykj <kjgodsey2@gmail.com>
2021-03-29 13:32:09 -0400

    Computing bias, part b question 1

--- Question1.R
+++ Question1.R
@@ -9,20 +9,29 @@ cor$fpc <- N
 n <- 58
 
 totx_a <-rep(NA,100)
+bias_a <-rep(NA,100)
+
+#actual death count
+truetotdeath <- sum(cor$Deaths)
 
 for(i in 101:200){
   set.seed(i)
   corsamp <- cor[sort(sample(N,n)),]
   corsampdes <- svydesign(id = ~1, strata = NULL, data = corsamp, fpc = ~fpc)
   sampdeathtot <- svytotal(~Deaths, design=corsampdes, na.rm=TRUE)
-  
+  bias <- sampdeathtot - truetotdeath
+  bias_a[i-100] <- bias
   totx_a[i-100] <- sampdeathtot
 }
 
 totx_a<-data.frame(totx_a)
+bias_a<-data.frame(bias_a)
 
-a <- ggplot(totx_a,aes(totx_a)) + geom_histogram(bins=30)
-a
+totaplot <- ggplot(totx_a,aes(totx_a)) + geom_histogram(bins=30)
+totaplot
+
+biasaplot <- ggplot(bias_a,aes(bias_a)) + geom_histogram(bins=30)
+biasaplot
 
 var_a<-var(totx_a)
 var_a
